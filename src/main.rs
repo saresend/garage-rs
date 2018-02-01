2b55b0b3892082ae29829aea5ab1665a0728f9ee
Samuel Resendez <saresend@usc.edu>
2018-01-31 18:29:02 -0800

    added match section

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,34 @@
-#[macro_use]
-extern crate quicli;
 
-use quicli::prelude::*;
+extern crate clap;
 
-
-#[derive(Debug, StructOpt)]
+use clap::{App, Arg};
 
 
 
 fn main() {
 
-    println!("Hello, world!");
+    let matches = App::new("Garage - quick CLI datastore")
+        .version("0.1")
+        .author("Samuel Resendez <saresend@usc.edu>")
+        .about("Easy way to store string values for later use!")
+        .arg(
+            Arg::with_name("CMD")
+                .help("Accepts either s for set variable, or g for get variable")
+                .required(true)
+                .index(1),
+        )
+        .arg(
+            Arg::with_name("KEYNAME")
+                .help("The Key Value to Get or Set")
+                .required(true)
+                .index(2),
+        )
+        .get_matches();
+
+
+
+
+
+
+
 }
